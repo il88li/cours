5340fd1b33655309d81e4e43f54565950118ccf1
il88li <yl9667383@gmail.com>
2026-02-26 17:26:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+TOKEN = "8531055332:AAGAT8Q7UMlyAHjOif1IJwyrZGcEZYLhmW4"
+CHANNEL_ID = -1003091756917          # قناة الفيديوهات الخاصة
+REQUIRED_CHANNEL = "@iIl337"         # قناة الاشتراك الإجباري
+ADMIN_IDS = [6689435577]              # معرف المدير (يمكن إضافة المزيد)
+DONATION_TARGET = "@OlIiIl7"          # حساب المدير لدعم النجوم 
